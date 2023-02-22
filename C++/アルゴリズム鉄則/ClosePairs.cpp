b4ad39f0867f3c6d324d63f22c72be7041967ddc
mizuki <sma0729mex@gmail.com>
2023-02-22 19:17:43 +0900

    commit

--- C++/アルゴリズム鉄則/ClosePairs.cpp
+++ C++/アルゴリズム鉄則/ClosePairs.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-#include <vector>
-#define _GLIBCXX_DEBUG
 using namespace std;
-//しゃくとり法
-int main(){
+
+int N, K;
+int A[100009], R[100009];
+
+int main() {
+	// 入力
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) cin >> A[i];
+	// しゃくとり法
+	for (int i = 1; i <= N - 1; i++) {
+        for(int i = 0; i < 10; i++) cout << R[i] << " ";
+        cout << endl;
+        cout << "------------" << endl;
+		// スタート地点を決める
+		if (i == 1) R[i] = 1;
+		else R[i] = R[i - 1];
+
+		// ギリギリまで増やしていく
+		while (R[i] < N && A[R[i] + 1] - A[i] <= K) {
+			R[i] += 1;
+		}
+	}
+
+	// 出力（答えは最大 50 億程度になるので long long 型を使う必要があります）
+	long long Answer = 0;
+	for (int i = 1; i <= N - 1; i++){
+        Answer += (R[i] - i);
+        cout << Answer << endl;
+    }
+	cout << Answer << endl;
+	return 0;
 }
