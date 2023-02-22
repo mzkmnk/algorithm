b65b06bbdf7df0ae50d9c064d0209610a4a37baf
mizuki <sma0729mex@gmail.com>
2023-02-22 22:03:22 +0900

    commit

--- C++/アルゴリズム鉄則/FourBoxes.cpp
+++ C++/アルゴリズム鉄則/FourBoxes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -27,9 +28,37 @@ int main(){
             cnt++;
         }
     }
-
+    // for(int i = 0; i < N*N+1; i++) cout << AB.at(i) << " ";
+    // cout << endl;
+    // for(int i = 0; i < N*N+1; i++) cout << CD.at(i) << " ";
+    // cout << endl;
+    
     sort(AB.begin(),AB.end());
     sort(CD.begin(),CD.end());
 
-    
+    for(int i = 1; i < AB.size(); i++){
+        int num = K - AB.at(i);
+        int left = 1, right = N*N,mid = (right + left) / 2;
+
+        // cout << "num = " << num << endl;
+
+        while(right > left){
+            if(num == CD.at(mid)){
+                left = mid;
+                break;
+            }
+            if(num > CD.at(mid)) left = mid + 1;
+            else right = mid - 1;
+            mid = (right + left) / 2;
+
+            // cout << "left = " << left << ", right = " << right << endl;
+            // cout << "left = " << CD.at(left) << "right = " << CD.at(right) << endl;
+
+        }
+        if(num == CD.at(mid)){
+            cout << "Yes" << endl;
+            exit(0);
+        }
+    }
+    cout << "No" << endl;
 }
--- C++/アルゴリズム鉄則/tempCodeRunnerFile.cpp
+++ C++/アルゴリズム鉄則/tempCodeRunnerFile.cpp
@@ -1,2 +1,15 @@
-000);
-    // cout << 1 << endl;
+
+    for(int i = 1; i < N*N+1; i++){
+        int num = K - AB.at(i);
+        int left = 1, right = N*N+1,mid = (right + left) / 2;;
+        while(right - left > 0){
+            if(num >= CD.at(mid)) left = mid;
+            else right = mid - 1;
+            mid = (right + left) / 2;
+        }
+        if(num == CD.at(mid)){
+            cout << mid << endl;
+            exit(0);
+        }
+    }
+    cout << "No" << endl;
