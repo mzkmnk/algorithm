0b1d131284ff49a42f15a546a2b03d05cfaea8a5
mizuki <sma0729mex@gmail.com>
2023-02-22 19:52:13 +0900

    commit

---
+++ C++/アルゴリズム鉄則/FourBoxes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#define _GLIBCXX_DEBUG
+using namespace std;
+
+
+int main(){
+    int N,K;
+    vector<int> A(N+1),B(N+1),C(N+1),D(N+1);
+    for(int i = 1; i < N+1; i++)
+}
--- C++/アルゴリズム鉄則/tempCodeRunnerFile.cpp
+++ C++/アルゴリズム鉄則/tempCodeRunnerFile.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 
-int main(){
-    int N,K;
-    cin >> N >> K;
-    vector<int> A(N);
-    for(int i=0;i<N;i++){
-        cin >> A.at(i);
-    }
-    int left = 1,right = 1000000000;
-    while(right - left > 0){
-        long long mid = (left+right)/2,sum = 0;
-        for(int i = 0; i < N; i++) sum += mid / A.at(i);
-        if(K <= sum) right = mid;
-        else left = mid + 1;
-        // cout << "left = " << left << " right = " << right << endl;
-    }
-    cout << left << endl;
-}
+int A[1009],B[1009],C[1009],D[1009];
+int P[1000000000],Q[1000000000];
