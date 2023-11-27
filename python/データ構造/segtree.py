6c54b08cb7e4070837d27ca604d351e77bd2c119
HidemaruOwO <sma0729mex@gmail.com>
2023-11-27 12:38:47 +0900

    segtree.pyに機能追加

--- python/データ構造/segtree.py
+++ python/データ構造/segtree.py
@@ -49,3 +49,47 @@ class SegTree:
         assert 0<=p<self._n
         return self._d[p+self._size]
 
+    #二分探索(条件を満たす最小の要素を求める)
+    def max_right(self, l, f):
+        assert 0 <= l <= self._n
+        assert f(self._e())
+        if l == self._n: return self._n
+        l += self._size # 葉に移動
+        sm = self._e() # 確定した区間の積を保持する変数
+        while True:
+            while l % 2 == 0: l >>= 1 #　右ノードになるまで
+            if not f(self._op(sm, self._d[l])):
+                # STEP2
+                while l < self._size:
+                    l <<= 1
+                    if f(self._op(sm, self._d[l])):
+                        sm = self._op(sm, self._d[l])
+                        l += 1
+                return l - self._size
+            sm = self._op(sm, self._d[l])
+            l += 1
+            if l & -l == l: break # f(prod(l, N))=Trueが確定
+        return self._n
+
+    #二分探索(条件を満たす最大の要素を求める)
+    def min_left(self, r, f):
+        assert 0 <= r <= self._n
+        assert f(self._e())
+        if r == 0: return 0
+        r += self._size
+        sm = self._e()
+        while True:
+            r -= 1
+            while r > 1 and r % 2: r >>= 1 # 左子ノードになるまで
+            if not f(self._op(self._d[r], sm)):
+                # STEP2
+                while r < self._size:
+                    r = 2 * r + 1 # 右子ノードに移動
+                    if f(self._op(self._d[r], sm)):
+                        sm = self._op(self._d[r], sm)
+                        r -= 1
+                return r + 1 - self._size
+            sm = self._op(self._d[r], sm)
+            if r & -r == r: break
+        return 0
+
